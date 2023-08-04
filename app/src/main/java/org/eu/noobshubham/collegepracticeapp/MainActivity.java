3ad684d5522c83f05ccbeaea08541823b1599a69
noobshubham <noobshubham65@gmail.com>
2023-08-04 21:44:38 +0530

    u: add two number

--- app/src/main/java/org/eu/noobshubham/collegepracticeapp/MainActivity.java
+++ app/src/main/java/org/eu/noobshubham/collegepracticeapp/MainActivity.java
@@ -2,7 +2,12 @@ package org.eu.noobshubham.collegepracticeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText numOne = (EditText) findViewById(R.id.numOneED);
+        EditText numTwo = (EditText) findViewById(R.id.numTwoED);
+        Button addButton = (Button) findViewById(R.id.add);
+        TextView total = (TextView) findViewById(R.id.result);
+
+        // string -> int
+        Integer numberOne = Integer.parseInt(numOne.getText().toString());
+        Integer numberTwo = Integer.parseInt(numTwo.getText().toString());
+
+        int result = numberOne + numberTwo;
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                total.setText("RESULT: " + result);
+            }
+        });
     }
 }
