1a3c655c0a68c4fa0384eb3e4bc65a1d721f3dcb
noobshubham <noobshubham65@gmail.com>
2023-08-06 14:54:26 +0530

    u: Code to Show Your Name on Screen & Change Image from Gallery

--- README.md
+++
@@ -1,9 +0,0 @@
-Project: CollegePracticeApp
-============================
-
-Android Programs
-------------------
-| Program                                                                       | Description            |
-|-------------------------------------------------------------------------------|------------------------|
-| [master](https://github.com/noobshubham/CollegePracticeApp)   | This branch            |
-| [add-num](https://github.com/noobshubham/CollegePracticeApp/tree/add-num) | Add Two Number Program |
--- app/src/main/java/org/eu/noobshubham/collegepracticeapp/MainActivity.java
+++ app/src/main/java/org/eu/noobshubham/collegepracticeapp/MainActivity.java
@@ -1,14 +1,37 @@
 package org.eu.noobshubham.collegepracticeapp;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    int SELECT_PICTURE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // code to chose the image from gallery
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(view -> imageChooser());
+
+        // code to set your name
+        TextView name = findViewById(R.id.textView);
+        name.setText("Enter Your Name Here!");
+    }
+
+    void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivity(Intent.createChooser(i, "Select Picture"));
     }
 }
